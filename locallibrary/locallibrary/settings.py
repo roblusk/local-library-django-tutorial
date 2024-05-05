b16b7a2086b65c8cc24fa09d7ddba5be8cf11a60
roblusk <130242918+roblusk@users.noreply.github.com>
2024-05-05 16:51:59 +0000

    fixed: moved catalog app to INSTALLED_APPS in settings.py

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "catalog.apps.CatalogConfig",
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "catalog.apps.CatalogConfig",
 ]
 
 ROOT_URLCONF = "locallibrary.urls"
