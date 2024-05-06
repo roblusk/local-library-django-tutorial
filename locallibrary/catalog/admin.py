65691156d9c025f67553f289b86d7235db6eba69
roblusk <130242918+roblusk@users.noreply.github.com>
2024-05-06 04:31:42 +0000

    added catalog models to admin panel; updated sample data script to include BookInstances

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Genre, Language, Book, BookInstance, Author
+
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Author)
--- locallibrary/create_sample_data.py
+++ locallibrary/create_sample_data.py
@@ -15,18 +15,21 @@ from catalog.models import Genre, Book, BookInstance, Author, Language
 # Initialize Faker instance
 fake = Faker()
 Faker.seed(1) # generate consistent sample data
+random.seed(1)
 
 # Create 6 Genres
 for _ in range(6):
     genre_name = fake.word()
     genre, _ = Genre.objects.get_or_create(name=genre_name)
     genre.save()
+print(f"There are now {Genre.objects.count()} genres in the database.")
 
 # Create 6 languages
 for _ in range(6):
     language_name = fake.language_name()
     language, _ = Language.objects.get_or_create(name=language_name)
     language.save()
+print(f"There are now {Language.objects.count()} languages in the database.")
 
 # Create 10 authors
 from datetime import date
@@ -36,6 +39,7 @@ for _ in range(10):
     date_of_birth = fake.date_of_birth(minimum_age=18, maximum_age=100)
     author, _ = Author.objects.get_or_create(first_name=first_name, last_name=last_name, date_of_birth=date_of_birth)
     author.save()
+print(f"There are now {Author.objects.count()} authors in the database.")
 
 # Create 50 books
 authors = Author.objects.all()
@@ -52,10 +56,21 @@ for _ in range(50):
         continue
 
     book, _ = Book.objects.get_or_create(title=title, author=author, summary=summary, isbn=isbn, language=language)
-    
-    selected_genres = random.sample(list(genres), k=random.randint(1,3))
-    for genre in selected_genres:
-        book.genre.add(genre)
+    for _ in range(random.randint(1, 3)):
+        book.genre.add(random.choice(genres))
     
     book.save()
+print(f"There are now {Book.objects.count()} books in the database.")
 
+books = Book.objects.all()
+# Create 250 book instances
+for _ in range(250):
+    book = random.choice(books)
+    imprint = fake.unique.random_number(digits=5)
+    due_back = random.choice(
+        [None,
+        fake.date_time_this_year(before_now=False, after_now=True)
+        ])
+    book_instance = BookInstance(book=book, imprint=imprint, due_back=due_back)
+    book_instance.save()
+print(f"There are now {BookInstance.objects.count()} book instances in the database.")
