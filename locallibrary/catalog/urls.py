0e1f288edc887846f131ca4240996491fa9d0981
roblusk <130242918+roblusk@users.noreply.github.com>
2024-05-08 06:14:10 +0000

    add url pattern for book create

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     #path('books/', views.books_list, name='books'),
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('book/create/', views.book_create, name="book-create"),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name="my-borrowed"),
