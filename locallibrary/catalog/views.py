777cf4cb53491bf12199148a3f71b293d4df2181
roblusk <130242918+roblusk@users.noreply.github.com>
2024-05-08 04:19:06 +0000

    Add authors list & view pages/functionality

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -33,6 +33,11 @@ from django.views import generic
 class BookListView(generic.ListView):
     model=Book
     paginate_by = 10
-
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model=Author
+    paginate_by = 10
+class AuthorDetailView(generic.DetailView):
+    model=Author
